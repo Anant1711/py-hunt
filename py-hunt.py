2b7007481efd7971e0472fc2bbfa80dd01edd886
Anant chauhan <63709820+Anant1711@users.noreply.github.com>
2020-10-22 07:27:31 -0400

    Update py-hunt.py

--- py-hunt.py
+++ py-hunt.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import time
 import datetime
 import socket
@@ -22,18 +23,18 @@ print("(_)@8@8@8{}_______|-\######/-|____"+(colored("By Anant","yellow",attrs=['
 print("       )_/        \          / ")
 print("      (@           `--------`  ")
 print('')
-print(colored("                       __                __","cyan"))
-print(colored("    ____  __  __      / /_  __  ______  / /_","cyan"))
-print(colored("   / __ \/ / / /_____/ __ \/ / / / __ \/ __/","cyan"))
-print(colored("  / /_/ / /_/ /_____/ / / / /_/ / / / / /_","cyan"))
-print(colored(" / .___/\__, /     /_/ /_/\__,_/_/ /_/\__/","cyan"))
-print(colored("/_/    /____/","cyan"))
+print(colored("\t                       __                __","cyan"))
+print(colored("\t    ____  __  __      / /_  __  ______  / /_","cyan"))
+print(colored("\t   / __ \/ / / /_____/ __ \/ / / / __ \/ __/","cyan"))
+print(colored("\t  / /_/ / /_/ /_____/ / / / /_/ / / / / /_","cyan"))
+print(colored("\t / .___/\__, /     /_/ /_/\__,_/_/ /_/\__/","cyan"))
+print(colored("\t/_/    /____/","cyan"))
 print('')
 print(colored("="*80,"red"))
 time.sleep(3)
 print(datetime.datetime.now())
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                                                                                                                 
 ip = (input(colored("Enter the IP: ","yellow")))
 s_ip = socket.gethostbyname(ip)
 print(colored(s_ip,"green"))
@@ -44,18 +45,45 @@ time.sleep(2)
 print(colored("_"*80,"red"))
 print(colored("[*] Scanning the ports and services: ","yellow"))
 print('')
-print(os.system("%s %s" % ("/usr/bin/nmap -sC -sV -p-",s_ip)))
+print(colored(" __   __   __  ___     __   __                  ___  __","magenta"))  
+print(colored("|__) /  \ |__)  |     /__` /  `  /\  |\ | |\ | |__  |__)","magenta"))
+print(colored("|    \__/ |  \  |     .__/ \__, /~~\ | \| | \| |___ |  \ ","magenta"))
+print('')
+subprocess.run(["/usr/bin/nmap", "-sC", "-sV","-p-", s_ip])
 print(colored("_"*80,"red"))
 
 # for finding hidden web directories
 print(colored("[*] Scanning Hidden Web Directories...","yellow"))
 print('')
+
+print(colored("      ___  __      __   __                  ___  __ ","magenta"))
+print(colored("|  | |__  |__)    /__` /  `  /\  |\ | |\ | |__  |__)","magenta"))
+print(colored("|/\| |___ |__)    .__/ \__, /~~\ | \| | \| |___ |  \ ","magenta"))
+print('')
 print(os.system("%s%s %s" % ("/usr/bin/gobuster -q dir -u http://",s_ip,"-w /usr/share/SecLists/Discovery/Web-Content/big.txt -x .txt -x .php -x .js -x .html")))
 print('')
 print(colored("_"*80,"red"))
+
+#smb
+try:
+        
+        print(colored(" __         __      ___                 ","magenta"))
+        print(colored("/__`  |\/| |__)    |__  |\ | |  |  |\/|","magenta"))
+        print(colored(".__/  |  | |__)    |___ | \| \__/  |  |","magenta")) 
+        print('')
+        print(colored("[*] Scanning the SMB services: ","yellow"))
+        print('')
+        subprocess.run(["enum4linux", s_ip])
+        print(colored("_"*80,"red"))
+except Exception as e:
+        print(str(e))
 # whois
 
 print(colored("[*] Whois information: ","yellow"))
+print(colored("           __  @  __ ","magenta"))
+print(colored("|  | |__| /  \ | /__`","magenta"))
+print(colored("|/\| |  | \__/ | .__/","magenta"))
+ 
 print('')
 print(os.system("%s %s " % ("whois " , s_ip )))
 print('')
